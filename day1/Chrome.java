9a986eae568f576d0c7beec8a2e041008bd480f8
tharani-rajesh <tharanithangaraj6@gmail.com>
2025-07-19 21:27:03 +0530

    Add files via upload
    Java operators

---
+++ day1/Chrome.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Chrome {
+public static void main(String[] args) {
+	float version=91.0f;
+	System.out.println(version);
+	String developer="Google";
+	System.out.println(developer);
+	boolean isBeta=false;
+	System.out.println(isBeta);
+	int releaseYear=2008;
+	System.out.println(releaseYear);
+	char shortcutKey='C';
+	System.out.println(shortcutKey);		
+}
+}
